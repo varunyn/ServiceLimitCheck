26c3f123c6e625c17cc2d9a4883cdac4208bab5d
Ryan Helferich <webdev2080@gmail.com>
2024-09-24 16:02:22 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# OCI Service Limit Checker Function using OCI Python SDK
+
+This function checks the service limits, usage and availability of all OCI resources across one or more regions in a tenancy. 
+Once the function executes it will send a summary to the subscribed emails within your designated OCI Notification Topic. 
+The function utilizes the OCI Python SDK and the OCI Functions Resource Principal for authentication.
+
+Example output: "Service: database, Scope: AD, AD: UWQV:US-ASHBURN-AD-1, Limit Name: vm-standard1-ocpu-count, Limit: 4, Usage: 3, Available: 1, Usage %: 75.00%"
+
+As you make your way through this tutorial, look out for this icon ![user input icon](./images/userinput.png).
+Whenever you see it, it's time for you to perform an action.
+
+## Prerequisites
+Before you deploy this sample function, make sure you have run step A, B and C of the [Oracle Functions Quick Start Guide for Cloud Shell](https://www.oracle.com/webfolder/technetwork/tutorials/infographics/oci_functions_cloudshell_quickview/functions_quickview_top/functions_quickview/index.html)
+* A - Set up your tenancy
+* B - Create application
+* C - Set up your Cloud Shell dev environment
+
+## List Applications 
+Assuming your have successfully completed the prerequisites, you should see your 
+application in the list of applications.
+```
+fn ls apps
+```
+
+## Create or Update your Dynamic Group
+In order to use other OCI Services, your function must be part of a dynamic group. For information on how to create a dynamic group, refer to the [documentation](https://docs.cloud.oracle.com/iaas/Content/Identity/Tasks/managingdynamicgroups.htm#To).
+
+When specifying the *Matching Rules*, we suggest matching all functions in a compartment with:
+```
+ALL {resource.type = 'fnfunc', resource.compartment.id = 'ocid1.compartment.oc1..aaaaaxxxxx'}
+```
+Please check the [Accessing Other Oracle Cloud Infrastructure Resources from Running Functions](https://docs.cloud.oracle.com/en-us/iaas/Content/Functions/Tasks/functionsaccessingociresources.htm) for other *Matching Rules* options.
+
+
+## Create or Update IAM Policies
+Now that your dynamic group is created, create a new policy that allows the
+dynamic group to use any resources you are interested in receiving
+information about, in this case we will grant access to `read all-resources` in
+the root compartment.
+
+![user input icon](./images/userinput.png)
+
+Your policy should look something like this:
+```
+Allow dynamic-group <dynamic-group-name> to read all-resources in tenancy 
+Allow dynamic-group <dynamic-group-name> to use ons-topics in tenancy 
+Allow dynamic-group <dynamic-group-name> to use ons-subscriptions in tenancy 
+```
+
+For more information on how to create policies, check the [documentation](https://docs.cloud.oracle.com/iaas/Content/Identity/Concepts/policysyntax.htm).
+
+
+## Create Notifications Topic 
+In order to recieve the summarized service limits of the tenancy, you'll need an OCI Notifications topic & Subscription for the data to be sent to. 
+![user input icon](./images/userinput.png)
+
+```
+[Create an OCI Topic](https://docs.oracle.com/en-us/iaas/Content/Notification/Tasks/create-topic.htm#top)
+
+Save the OCID of the OCI Topic you just created for use in the function. 
+
+[Create an OCI Email Subscription](https://docs.oracle.com/en-us/iaas/Content/Notification/Tasks/create-subscription-email.htm#top)
+
+NOTE - You must confirm the subscription email in your inbox after the subscription is created. 
+```
+
+## Required - Customize the test.json file
+- notification_topic_id: "<Topic OCID>"
+- Optional - regions (Default is home region)
+- Optional - threshold_percentage (Default is 90)
+
+## Optional - Review and customize the function
+Review the following files in the current folder:
+* the code of the function, [func.py](./func.py)
+* its dependencies, [requirements.txt](./requirements.txt)
+* the function metadata, [func.yaml](./func.yaml)
+
+## Deploy the function
+In Cloud Shell, run the *fn deploy* command to build the function and its dependencies as a Docker image, 
+push the image to OCIR, and deploy the function to Oracle Functions in your application.
+
+![user input icon](./images/userinput.png)
+```
+fn -v deploy --app <app-name>
+```
+
+## Invoke the function
+
+The function requires the following keys in the payload when invoked:
+- *REQUIRED* - "notification_topic_id", the OCID of Notification Topic to send the summary.
+- *Optional* - "threshold_percentage", the percentage threshold for which resources are nearing their limit. (Default is 90%)
+- *Optional* - "regions", the specific regions to query. (Default is tenancy home region). - NOTE: (If querying a large amount of regions, function may time out due to amount of data being pulled). 
+
+To run the function, you will need to invoke it with at least the notification_topic_id payload. 
+
+![user input icon](./images/userinput.png)
+```
+fn invoke <app-name> <function-name> < test.json
+```
+e.g.:
+```
+fn invoke ServiceLimitApp ServiceLimitFunction < test.json
+```
+
+Assuming the other function was invoked correctly, you should see the following output after a few minutes :
+```json
+{"message": "Function executed successfully"}
+```
+
+Shortly after, the Notification Topic subscribers will recieve a message (email) with the summary of the resources sitting above the usage threshold. 
+
+- *Optional* - You can run the OCI Function from any OCI CLI Authorized device with a Function Invoke Endpoint. Docs: https://docs.oracle.com/en-us/iaas/Content/Functions/Tasks/functionsinvokingfunctions.htm#rawrequestinvoke
+```
+e.g.: 
+oci raw-request --http-method POST --target-uri <invoke-endpoint> --request-body "<request-parameters>"
+```
---
+++ func.py
@@ -0,0 +1,295 @@
+import oci
+import datetime
+import time
+import json
+import logging
+import io
+
+from fdk import response
+
+# Set the default usage threshold percentage
+DEFAULT_THRESHOLD_PERCENTAGE = 90
+
+# Initialize summary buffer for log entries
+summary_buffer = []
+error_buffer = []
+
+# Function to log messages to both console and buffer (to be sent via email)
+def log_message(message):
+    logging.getLogger().info(message)
+    summary_buffer.append(message)
+
+# Function to add errors to the error log buffer (to be optionally sent via email)
+def add_to_error_log(message):
+    error_buffer.append(message)
+
+# Function to send the summary via OCI Notifications
+def send_notification(notification_topic_id, signer):
+    summary_message = "\n".join(summary_buffer)
+    error_message = "\n".join(error_buffer)
+
+    # Check if the summary message is empty
+    if not summary_message:
+        summary_message = "No resources are near their usage limits."
+
+    notification_message = f"OCI Resource Usage Report - {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n"
+    
+    # Append errors to the notification only if there are errors
+    if error_message:
+        notification_message += f"Errors encountered:\n{error_message}\n\n"
+
+    # Append the resource usage summary
+    notification_message += summary_message
+
+    notification_client = oci.ons.NotificationDataPlaneClient({}, signer=signer)
+    try:
+        response = notification_client.publish_message(
+            topic_id=notification_topic_id,
+            message_details=oci.ons.models.MessageDetails(
+                title="OCI Resource Usage Alert",
+                body=notification_message
+            )
+        )
+        #log_message(f"Notification sent successfully: {response.data}")
+    except oci.exceptions.ServiceError as e:
+        # Log and capture the error in the error log
+        error_message = f"Failed to send notification: {str(e)}"
+        log_message(error_message)
+        add_to_error_log(error_message)
+
+# Function to list all available services
+def list_all_services(compartment_id, limits_client, limit=1000):
+    services = []
+    next_page = None
+    while True:
+        services_response = limits_client.list_services(
+            compartment_id=compartment_id,
+            page=next_page,
+            limit=limit
+        )
+        services.extend(services_response.data)
+        
+        next_page = services_response.headers.get('opc-next-page')
+        if not next_page:
+            break
+    return services
+
+# Function to list all availability domains in the tenancy
+def list_availability_domains(identity_client, compartment_id):
+    availability_domains = []
+    ads_response = identity_client.list_availability_domains(compartment_id=compartment_id)
+    availability_domains.extend([ad.name for ad in ads_response.data])
+    return availability_domains
+
+# Function to get all limit definitions for a specific service
+def get_all_limit_definitions(service_name, compartment_id, limits_client, limit=1000):
+    limit_definitions = []
+    next_page = None
+    while True:
+        limit_definitions_response = limits_client.list_limit_definitions(
+            compartment_id=compartment_id,
+            service_name=service_name,
+            page=next_page,
+            limit=limit
+        )
+
+        limit_definitions.extend(limit_definitions_response.data)
+        next_page = limit_definitions_response.headers.get('opc-next-page')
+        if not next_page:
+            break
+    return limit_definitions
+
+# Function to get all limit values for a service
+def get_all_limit_values(service_name, compartment_id, limits_client, limit=1000):
+    limit_values = []
+    next_page = None
+    while True:
+        limit_values_response = limits_client.list_limit_values(
+            compartment_id=compartment_id,
+            service_name=service_name,
+            page=next_page,
+            limit=limit
+        )
+
+        limit_values.extend(limit_values_response.data)
+        next_page = limit_values_response.headers.get('opc-next-page')
+        if not next_page:
+            break
+    return limit_values
+
+# Function to get resource availability based on scope type
+def get_resource_availability(service_name, limit_name, compartment_id, limits_client, availability_domain=None, scope_type=None):
+    try:
+        if scope_type == 'AD':
+            # For AD-scoped resources, include availability_domain
+            availability_data = limits_client.get_resource_availability(
+                compartment_id=compartment_id,
+                service_name=service_name,
+                limit_name=limit_name,
+                availability_domain=availability_domain
+            ).data
+        else:
+            # For REGION or GLOBAL scoped resources, exclude availability_domain
+            availability_data = limits_client.get_resource_availability(
+                compartment_id=compartment_id,
+                service_name=service_name,
+                limit_name=limit_name
+            ).data
+
+        return availability_data.used, availability_data.available
+    except oci.exceptions.ServiceError as e:
+        log_message(f"Error fetching resource availability: {str(e)}")
+        return None, None
+
+# Function to log usage if above threshold into a summary buffer
+def log_usage_if_above_threshold(service_name, scope_type, availability_domain, limit_name, service_limit, usage, available, threshold_percentage):
+    if service_limit > 0:  # To avoid division by zero
+        usage_percentage = (usage / service_limit) * 100
+        if usage_percentage >= threshold_percentage:
+            # Log the resource if usage exceeds the threshold
+            log_entry = (f"Service: {service_name}, Scope: {scope_type}, AD: {availability_domain or 'N/A'}, "
+                         f"Limit Name: {limit_name}, Limit: {service_limit}, Usage: {usage}, Available: {available}, "
+                         f"Usage %: {usage_percentage:.2f}%")
+            
+            log_message(log_entry)  # Add to the summary buffer
+
+# Function to find the tenancy's home region
+def get_home_region(identity_client, tenancy_id):
+    regions = identity_client.list_region_subscriptions(tenancy_id).data
+    for region in regions:
+        if region.is_home_region:
+            return region.region_name
+    return None
+
+# Function to check service limits in the specified regions
+def check_service_limits(signer, notification_topic_id, regions, threshold_percentage):
+    try:
+        # Identity client to fetch tenancy (auth through Resource Principal)
+        identity_client = oci.identity.IdentityClient({}, signer=signer)
+
+        # Get the tenancy and root compartment ID from the Resource Principal signer
+        tenancy_id = signer.tenancy_id
+        compartment_id = tenancy_id  # Root compartment is same as tenancy ID
+
+        # Process the provided regions
+        if regions == 'all':
+            # Get list of all subscribed regions
+            regions = identity_client.list_region_subscriptions(tenancy_id).data
+            region_names = [r.region_name for r in regions]
+        elif isinstance(regions, list):
+            # Use the list of regions provided by the user
+            region_names = regions
+        else:
+            # Process a single region if provided as a string
+            region_names = [regions]
+
+        # Iterate through each region
+        for region_name in region_names:
+            log_message(f"Processing region: {region_name}")
+
+            # Update the signer to use the current region
+            config = {"region": region_name}
+            limits_client = oci.limits.LimitsClient(config, signer=signer)
+
+            # List all services in the compartment (tenancy)
+            services = list_all_services(compartment_id, limits_client)
+
+            #log_message(f"Services found in region {region_name}: {len(services)}")
+
+            # Get the list of Availability Domains (ADs) for the tenancy
+            availability_domains = list_availability_domains(identity_client, compartment_id)
+
+            # Iterate through each service
+            for service in services:
+                service_name = service.name
+
+                # Get all limit definitions for the service
+                limit_definitions = get_all_limit_definitions(service_name, compartment_id, limits_client)
+
+                for definition in limit_definitions:
+                    limit_name = definition.name
+                    scope_type = definition.scope_type
+
+                    # Skip deprecated limit definitions
+                    if definition.is_deprecated:
+                        continue
+
+                    # Get all limit values for this service and limit name
+                    limit_values = get_all_limit_values(service_name, compartment_id, limits_client)
+
+                    for limit_value_obj in limit_values:
+                        if limit_value_obj.name == limit_name:
+                            service_limit = limit_value_obj.value
+
+                            # If the resource scope is AD, loop through all ADs
+                            if scope_type == 'AD':
+                                for ad in availability_domains:
+                                    # Get resource availability for the specific AD
+                                    usage, available = get_resource_availability(
+                                        service_name, limit_name, compartment_id, limits_client, ad, scope_type='AD'
+                                    )
+
+                                    if usage is not None and available is not None:
+                                        # Log the resource if usage exceeds the threshold
+                                        log_usage_if_above_threshold(service_name, scope_type, ad, limit_name, service_limit, usage, available, threshold_percentage)
+                            else:  # REGION or GLOBAL scoped limits
+                                # Get resource availability without AD
+                                usage, available = get_resource_availability(
+                                    service_name, limit_name, compartment_id, limits_client, scope_type=scope_type
+                                )
+
+                                if usage is not None and available is not None:
+                                    # Log the resource if usage exceeds the threshold
+                                    log_usage_if_above_threshold(service_name, scope_type, None, limit_name, service_limit, usage, available, threshold_percentage)
+
+        # Send the summary notification
+        send_notification(notification_topic_id, signer)
+
+        return {"message": "Function executed successfully."}
+    
+    except Exception as ex:
+        error_message = f"Function execution failed: {str(ex)}"
+        log_message(error_message)
+        add_to_error_log(error_message)
+        return {"error": error_message}
+
+# Main handler function for OCI Functions
+def handler(ctx, data: io.BytesIO = None):
+    try:
+        # Parse the event input (expecting notification_topic_id, regions, and threshold_percentage in the event)
+        event = json.loads(data.getvalue())
+        notification_topic_id = event.get("notification_topic_id")
+        regions = event.get("regions")  # User-specified regions as list or "all"
+        threshold_percentage = event.get("threshold_percentage", DEFAULT_THRESHOLD_PERCENTAGE)  # Use provided or default to 90%
+
+        if not notification_topic_id:
+            log_message("Error: Notification topic ID not provided.")
+            return response.Response(
+                ctx, response_data=json.dumps({"error": "Notification topic ID not provided"}), 
+                headers={"Content-Type": "application/json"}
+            )
+
+        # Initialize Resource Principal authentication (signer)
+        signer = oci.auth.signers.get_resource_principals_signer()
+
+        # If no regions are provided, default to home region
+        if not regions:
+            identity_client = oci.identity.IdentityClient({}, signer=signer)
+            regions = get_home_region(identity_client, signer.tenancy_id)
+            log_message(f"Defaulting to home region: {regions}")
+
+        # Execute the logic
+        resp = check_service_limits(signer, notification_topic_id, regions, threshold_percentage)
+
+        return response.Response(
+            ctx, response_data=json.dumps(resp),
+            headers={"Content-Type": "application/json"}
+        )
+    
+    except Exception as ex:
+        error_message = f"Function execution failed: {str(ex)}"
+        log_message(error_message)
+        return response.Response(
+            ctx, response_data=json.dumps({"error": error_message}),
+            headers={"Content-Type": "application/json"}
+        )
